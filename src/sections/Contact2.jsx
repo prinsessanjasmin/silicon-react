f9902ff5f36076fa33273e166e93adf922c1c54d
Jasmin de Freitas <jasmindefreitas@hotmail.com>
2024-11-11 08:23:48 +0100

    Commit 12: Minor changes in design.

--- src/sections/Contact2.jsx
+++ src/sections/Contact2.jsx
@@ -10,8 +10,10 @@ function Contact2() {
                         <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d12621.335361536538!2d-122.40866839022975!3d37.73531158510237!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sse!4v1729170961143!5m2!1sen!2sse" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
                     </div>
                     <div className="location-box">
-                        <Location />
-                        <Location />
+                        <div className="locations-div">
+                            <Location />
+                            <Location />
+                        </div>                    
                         <SocialMediaLogos />
                     </div>
                 </div>
